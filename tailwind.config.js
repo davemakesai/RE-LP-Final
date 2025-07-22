1967c27dffd1af43b2a48f6f6741126bddb1fde5
davemakesai <davemakesai@gmail.com>
2025-07-22 18:28:04 +0530

    Apply Montserrat Font Site-wide

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['Montserrat', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif'],
+      },
       animation: {
         'scroll-left': 'scroll-left 30s linear infinite',
         'scroll-right': 'scroll-right 30s linear infinite',
